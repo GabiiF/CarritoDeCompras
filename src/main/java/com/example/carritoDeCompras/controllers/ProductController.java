af72b8b4b7984a26c3c1544555b7f5e5674e5ec6
GabiiF <gabriela.fernandez.92@live.com.ar>
2023-07-16 06:04:19 -0300

    creo funcion de editar y creo html de editar_producto, falta cambiarlo

--- src/main/java/com/example/carritoDeCompras/controllers/ProductController.java
+++ src/main/java/com/example/carritoDeCompras/controllers/ProductController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -34,10 +36,20 @@ public class ProductController {
        return "nuevo_producto";
    }
     @RequestMapping(value="/guardar", method= RequestMethod.POST)
-    //@ModelAttribute= atributo modelo que es el th:object=producto
+    //@ModelAttribute-> atributo modelo que es el th:object=producto
     public String guardarProducto(@ModelAttribute("producto") Producto producto){
         productService.save(producto);//guardamos
         return "redirect:/";
+    }
+    @RequestMapping("/editar-producto/{id}")
+    //@PathVariable-> variable que le paso por URL
+    public ModelAndView editarProducto(@PathVariable(name="id") Long id){
+       ModelAndView model= new ModelAndView("editar_producto");
+
+       //obtiene el id
+       Producto producto= productService.get(id);
+       model.addObject("producto",producto);
+       return model;
     }
 
 }
